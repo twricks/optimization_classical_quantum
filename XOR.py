978a29fc3d6358738a314eecc0c36728d99e8d58
Tom R <thomaswricksjr@gmail.com>
2026-04-19 14:27:31 -0400

    Add files via upload

--- XOR.py
+++ XOR.py
@@ -8,9 +8,10 @@ import random
 m = 2
 delta = 2
 iterations = 4000
-correlator = 0.99 #correlation strength of x and y
+correlator = 0.5 #correlation strength of x and y; 0.5 -> independent, <0.5 means negative correlation, >0.5 positive correlation. 
 #each row of counts is an output pair, each column an input pair
-counts = np.zeros((4, 4), dtype=int)
+q_counts = np.zeros((4, 4), dtype=int)
+c_counts = np.zeros((4, 4), dtype=int)
 
 sim = AerSimulator()
 
@@ -22,17 +23,33 @@ def Referee(correlator):
         y = random.randint(0, 1)
     return x, y
 
-def Alice(x, qc):
+def qAlice(x, qc):
     if x == 0:
+        pass
+    else:
         qc.h(0)
-    qc.measure(0, 0)
+    qc.measure(0, 0) #we don't need a return statement because Alice has saved her result in the first classical bit
 
-def Bob(y, qc):
+def qBob(y, qc):
     if y == 0:
         qc.ry(-np.pi/4, 1)  # (Z + X)/√2
     else:
         qc.ry(np.pi/4, 1)   # (Z - X)/√2
     qc.measure(1, 1)
+
+def cAlice(x):
+    return 0
+
+def cBob(y):
+    #classical strategy- they buy nothing. This succeeds 75% of the time, since addmition mod 2 of the outputs has to match AND condition of the inputs
+    return 0
+
+#utility function block
+def is_win(a,b,x,y):
+    return 1 if (a ^ b) == (x & y) else 0
+
+q_wins = 0
+c_wins = 0
 
 for _ in range(iterations):
     qc = QuantumCircuit(2, 2)
@@ -42,10 +59,10 @@ for _ in range(iterations):
     qc.cx(0, 1)
 
     x, y = Referee(correlator)
-    Alice(x, qc)
-    Bob(y, qc)
+    qAlice(x, qc)
+    qBob(y, qc)
 
-    result = sim.run(qc,shots=200).result()
+    result = sim.run(qc,shots=1).result()
     counts_dict = result.get_counts(qc)
     bitstring = next(iter(counts_dict))
 
@@ -57,28 +74,43 @@ for _ in range(iterations):
     input_idx = 2 * x + y
     output_idx = 2 * a + b
 
-    counts[input_idx, output_idx] += 1
+    q_counts[input_idx, output_idx] += 1
+    q_wins += is_win(a,b,x,y)
 
+    #now classical round for comparison
+    a_c = cAlice(x)
+    b_c = cBob(y)
+    output_idx_c = 2* a_c * b_c
+    c_counts[input_idx,output_idx_c] += 1
+    c_wins += is_win(a_c,b_c,x,y)
+
+quantum_win_rate = q_wins / iterations
+classical_win_rate = c_wins / iterations
+
+print(f"Correlation strength: {correlator}")
+print(f"Quantum win rate: {quantum_win_rate}")
+print(f"Classical win rate: {classical_win_rate}")
+print(f"Quantum Advantage: {quantum_win_rate-classical_win_rate}")
 # ---- plotting ----
-fig, ax = plt.subplots(figsize=(6, 5))
-im = ax.imshow(counts, cmap="coolwarm")
+#fig, ax = plt.subplots(figsize=(6, 5))
+#im = ax.imshow(q_counts, cmap="coolwarm")
 
-plt.colorbar(im, ax=ax)
+#plt.colorbar(im, ax=ax)
 
-ax.set_xlabel("Output pair (a,b)")
-ax.set_ylabel("Input pair (x,y)")
+#ax.set_xlabel("Output pair (a,b)")
+#ax.set_ylabel("Input pair (x,y)")
 
-labels = ["00", "01", "10", "11"]
-ax.set_xticks(range(4))
-ax.set_yticks(range(4))
-ax.set_xticklabels(labels)
-ax.set_yticklabels(labels)
+#labels = ["00", "01", "10", "11"]
+#ax.set_xticks(range(4))
+#ax.set_yticks(range(4))
+#ax.set_xticklabels(labels)
+#ax.set_yticklabels(labels)
 
-for i in range(4):
-    for j in range(4):
-        ax.text(j, i, counts[i, j],
-                ha="center", va="center",
-                color="white" if counts[i, j] > np.max(counts)/2 else "black")
+#for i in range(4):
+ #   for j in range(4):
+  #      ax.text(j, i, q_counts[i, j],
+   #             ha="center", va="center",
+    #            color="white" if q_counts[i, j] > np.max(q_counts)/2 else "black")
 
-plt.tight_layout()
-plt.show()
+#plt.tight_layout()
+#plt.show()
